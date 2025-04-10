5eb31734288eb8c93e97062ed6742e02575ab633
AustiN-BU <gnelson1@mail.bradley.edu>
2025-04-10 16:53:22 -0500

    BossAi Laid out
    Still need to update Input System and finish up Enemy AI

--- Assets/Scripts/BossAI.cs
+++ Assets/Scripts/BossAI.cs
@@ -16,4 +16,47 @@ public class BossAI : MonoBehaviour
     public float timeBetweenAttacks;
     bool alreadyAttacked;
 
+    public float sightRange, attackRange;
+
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void Update()
+    {
+        //Checks for Sight and Attack Range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
+    }
+   
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void AttackPlayer()
+    {
+        //Makes sure the boss doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //The attack Code goes here
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
 }
